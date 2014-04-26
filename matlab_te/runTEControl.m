a6efb6cfb8291cd1e162aa21b91e4eaa2642f887
Gregory Nazario <gnazario@ieee.org>
2014-04-26 18:34:17 -0400

    Added TE controller and matlab packet controls

---
+++ matlab_te/runTEControl.m
@@ -0,0 +1,52 @@
+%% Format for packets
+% HEADER => Type of request
+% CHANGE -> Change the inputs (or in this case outputs of TE)
+% UPDATE -> Update and get the latest version of the controls
+% 
+%
+%
+% ALWAYS ALWAYS ALWAYS
+% node = int id
+% All packets need
+% dt = change in time since last sent packet
+% id = identifier of controller, starts at 1
+%
+
+
+%% Initialize controllers
+numControl = 1;
+i = 1;
+control = [];
+while (i <= numControl)
+    control(i) = SimpleController();
+    i = i + 1;
+end
+
+
+%% Startup pipe on port 18100
+pipe = OMNeTPipe('localhost', 18100);
+
+
+%% Recieve and iterate on packets
+while (1 == 1)
+    [header, pkMap] = recvPk(pipe);
+    
+    id = pkMap('id');
+    dt = pkMap('dt');
+    curControl = control(id);
+    
+    % Do a change of inputs and outputs
+    if (STRCMP(header, 'CHANGE'))      
+        % Update the controls
+        y = [pkMap('F_1'), pkMap('F_2'). pkMap('F_3'), pkMap('F_4'), pkMap('P'), pkMap('V_L'), pkMap('y_a3'), pkMap('y_b3'), pkMap('y_c3'), pkMap('C')];
+        u = updateControls(curControl, y, dt);
+        
+        % Send a packet containing the new input controls
+        sendPk(pipe, 'CH_ACK', id, pipe.typeInt, 'id', single(u(1)), pipe.typeFloat, 'u1', single(u(2)), pipe.typeFloat, 'u2',single(u(3)), pipe.typeFloat, 'u3', single(u(4)), pipe.typeFloat, 'u4');
+    % Get the inputs given the previous information
+    elseif (STRCMP(header, 'UPDATE'))
+        u = updateControls(curControl, getLastInputsFromController(curControl), dt);
+        sendPk(pipe, 'UP_ACK', id, pipe.typeInt, 'id', single(u(1)), pipe.typeFloat, 'u1', single(u(2)), pipe.typeFloat, 'u2',single(u(3)), pipe.typeFloat, 'u3', single(u(4)), pipe.typeFloat, 'u4');
+    end
+    
+end
---
+++ matlab_te/runTESim.m
@@ -0,0 +1,52 @@
+%% Format for packets
+% HEADER => Type of request
+% CHANGE -> Change the inputs
+% UPDATE -> Update and get the latest version of the controls
+% 
+%
+%
+% ALWAYS ALWAYS ALWAYS
+% node = int id
+% All packets need
+% dt = change in time since last sent packet
+% id = identifier of system, starts at 1
+%
+
+
+%% Initialize TE
+numTE = 1;
+i = 1;
+TE = [];
+while (i <= numTE)
+    TE(i) = SimpleTE();
+    i = i + 1;
+end
+
+
+%% Startup pipe on port 18100
+pipe = OMNeTPipe('localhost', 18240);
+
+
+%% Recieve and iterate on packets
+while (1 == 1)
+    [header, pkMap] = recvPk(pipe);
+    
+    id = pkMap('id');
+    dt = pkMap('dt');
+    curTE = TE(id);
+    
+    % Do a change of inputs and outputs
+    if (STRCMP(header, 'CHANGE'))      
+        % Update the controls
+        u = [pkMap('u1'), pkMap('u2'), pkMap('u3'), pkMap('u4')];
+        y = runIteration(curTE, u, dt);
+        
+        % Send a packet containing the new input controls
+        sendPk(pipe, 'CH_ACK', id, pipe.typeInt, 'id', single(outputs(1)), pipe.typeFloat, 'F_1', single(outputs(2)), pipe.typeFloat, 'F_2', single(outputs(3)), pipe.typeFloat, 'F_3', single(outputs(4)), pipe.typeFloat, 'F_4', single(outputs(5)), pipe.typeFloat, 'P', single(outputs(6)), pipe.typeFloat, 'V_L', single(outputs(7)), pipe.typeFloat, 'y_a3', single(outputs(8)), pipe.typeFloat, 'y_b3', single(outputs(9)), pipe.typeFloat, 'y_c3', single(outputs(10)), pipe.typeFloat, 'C');
+    % Get the inputs given the previous information
+    elseif (STRCMP(header, 'UPDATE'))
+        y = runIteration(curTE, getLastInputs(curTE), dt);
+        sendPk(pipe, 'UP_ACK', id, pipe.typeInt, 'id', single(outputs(1)), pipe.typeFloat, 'F_1', single(outputs(2)), pipe.typeFloat, 'F_2', single(outputs(3)), pipe.typeFloat, 'F_3', single(outputs(4)), pipe.typeFloat, 'F_4', single(outputs(5)), pipe.typeFloat, 'P', single(outputs(6)), pipe.typeFloat, 'V_L', single(outputs(7)), pipe.typeFloat, 'y_a3', single(outputs(8)), pipe.typeFloat, 'y_b3', single(outputs(9)), pipe.typeFloat, 'y_c3', single(outputs(10)), pipe.typeFloat, 'C');
+    end
+    
+end
